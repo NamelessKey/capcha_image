539bff983b9b800f3c0e7aaf71fe54e2ed498685
NamelessKey <nott.free@hotmail.com>
2014-02-09 10:18:14 +0700

    Second push project

--- Cap_Img.php
+++ Cap_Img.php
@@ -9,19 +9,19 @@
 
     $width = 50;
     $height = 24; 
-    
+
     $image = ImageCreate($width, $height);  
     $white = ImageColorAllocate($image, 255, 255, 255);
     $black = ImageColorAllocate($image, 0, 100, 0);
     $grey = ImageColorAllocate($image, 204, 204, 204);
-     
+
     ImageFill($image, 0, 0, $white); 
     //Add randomly generated string in white to the image
     ImageString($image, 10, 4, 4, $security_code, $black); 
-   // ImageRectangle($image,0,16,$width-1,$height-1,$grey); 
-   // imageline($image, 0, $height/2, $width, $height/2, $grey); 
-   // imageline($image, $width/2, 0, $width/2, $height, $grey); 
-	
+    // ImageRectangle($image,0,16,$width-1,$height-1,$grey); 
+    // imageline($image, 0, $height/2, $width, $height/2, $grey); 
+    // imageline($image, $width/2, 0, $width/2, $height, $grey); 
+
     header("Content-Type: image/jpeg"); 
     ImageJpeg($image);
     ImageDestroy($image);
--- validate.php
+++ validate.php
@@ -1,22 +1,22 @@
 <form name="form1" method="post" action="index.php">
-	<div align="center">
-		<input name="Submit" type="submit" value="back">
-	</div>
+    <div align="center">
+        <input name="Submit" type="submit" value="back">
+    </div>
 </form>
 <div align="center">
 <?php
-	@session_start();
-	$key=$_SESSION['count'];
+    @session_start();
+    $key=$_SESSION['count'];
 
-	$imag = $_POST['number'];
-	$user = md5($imag);
-	//echo "$imag  =  =  = $key<br>";
-	//echo("$user");
-	if($user==$key)	{
-		echo ("Verification success");
-	}
-	else{
-		echo "You have entered wrong verification code!!<br> Please go back and enter proper value.";
-	}
+    $imag = $_POST['number'];
+    $user = md5($imag);
+    //echo "$imag  =  =  = $key<br>";
+    //echo("$user");
+    if($user==$key)	{
+        echo ("Verification success");
+    }
+    else{
+        echo "You have entered wrong verification code!!<br> Please go back and enter proper value.";
+    }
 ?>
 </div>
